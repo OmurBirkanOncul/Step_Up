72b69d269d371ad3aacfc307287f3de82d66f6b4
MelisBuyukataman <melis.buyukataman@hotmail.com>
2024-12-28 14:57:37 +0300

    CreateAccount

--- app/src/main/java/com/example/stepup/MainActivity.kt
+++ app/src/main/java/com/example/stepup/MainActivity.kt
@@ -16,7 +16,7 @@ import com.example.stepup.ui.screens.LoginScreen
 import com.example.stepup.ui.theme.StepUpTheme
 
 
-class MainActivity : ComponentActivity() {
+/*class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -25,5 +25,5 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-}
+}*/
 
